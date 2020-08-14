4e6eac8b36836c373b20c835a6c0ab327c043b21
valemarti <valemarti@users.noreply.github.com>
2020-08-14 15:07:54 -0300

    Entrega 1

--- js/products.js
+++ js/products.js
@@ -41,8 +41,8 @@ function showCategoriesList(){
     for(let i = 0; i < currentCategoriesArray.length; i++){
         let category = currentCategoriesArray[i];
 
-        if (((minCount == undefined) || (minCount != undefined && parseInt(category.productCount) >= minCount)) &&
-            ((maxCount == undefined) || (maxCount != undefined && parseInt(category.productCount) <= maxCount))){
+        if (((minCount == undefined) || (minCount != undefined && parseInt(category.soldCount) >= minCount)) &&
+            ((maxCount == undefined) || (maxCount != undefined && parseInt(category.soldCount) <= maxCount))){
 
             htmlContentToAppend += `
             <a href="category-info.html" class="list-group-item list-group-item-action">
@@ -52,8 +52,8 @@ function showCategoriesList(){
                     </div>
                     <div class="col">
                         <div class="d-flex w-100 justify-content-between">
-                            <h4 class="mb-1">`+ category.name +`</h4>
-                            <small class="text-muted">` + category.productCount + ` artículos</small>
+                            <h4 class="mb-1">`+ category.name +  category.currency + category.cost +`</h4>
+                            <small class="text-muted">` + category.soldCount + ` artículos</small>
                         </div>
                         <p class="mb-1">` + category.description + `</p>
                     </div>
